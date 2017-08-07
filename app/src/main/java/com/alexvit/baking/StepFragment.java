51a38967fb7adf7353155ca6fc8c267d007d66e1
Alex Vit <ryujo.la@gmail.com>
2017-08-08 02:41:45 +0300

    bug: Video fixes

--- app/src/main/java/com/alexvit/baking/StepFragment.java
+++ app/src/main/java/com/alexvit/baking/StepFragment.java
@@ -11,18 +11,13 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.alexvit.baking.entity.Step;
-import com.google.android.exoplayer2.ExoPlaybackException;
-import com.google.android.exoplayer2.ExoPlayer;
+import com.alexvit.baking.view.AspectRatioFrameLayout;
 import com.google.android.exoplayer2.ExoPlayerFactory;
-import com.google.android.exoplayer2.PlaybackParameters;
 import com.google.android.exoplayer2.SimpleExoPlayer;
-import com.google.android.exoplayer2.Timeline;
 import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
 import com.google.android.exoplayer2.source.ExtractorMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
-import com.google.android.exoplayer2.source.TrackGroupArray;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
-import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 
@@ -43,7 +38,9 @@ public class StepFragment extends Fragment {
     TextView tvNumber;
     @BindView(R.id.step_short_description)
     TextView tvShortDescription;
-    @BindView(R.id.step_video)
+    @BindView(R.id.player_container)
+    AspectRatioFrameLayout mPlayerContainer;
+    @BindView(R.id.player_view)
     SimpleExoPlayerView mPlayerView;
     @BindView(R.id.step_description)
     TextView tvDescription;
@@ -72,6 +69,13 @@ public class StepFragment extends Fragment {
         return fragment;
     }
 
+    private static Bundle buildArgsBundle(List<Step> steps, int position) {
+        Bundle args = new Bundle();
+        args.putParcelableArrayList(KEY_ARG_STEPS, new ArrayList<>(steps));
+        args.putInt(KEY_ARG_POSITION, position);
+        return args;
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -89,8 +93,10 @@ public class StepFragment extends Fragment {
         mSteps = getSteps(getArguments(), savedInstanceState);
         mPosition = getPosition(getArguments(), savedInstanceState);
 
+        if (mPlayer == null)
+            mPlayer = ExoPlayerFactory.newSimpleInstance(getContext(), new DefaultTrackSelector());
+
         setPosition(mPosition);
-        updateUi();
     }
 
     @Override
@@ -138,15 +144,13 @@ public class StepFragment extends Fragment {
         );
 
         String videoUrl = step.videoURL;
-        if (videoUrl != null && !videoUrl.isEmpty()) initPlayer(videoUrl);
+        if (videoUrl != null && !videoUrl.isEmpty()) showVideo(videoUrl);
     }
 
-    private void initPlayer(String urlString) {
+    private void showVideo(String urlString) {
 
         Uri uri = Uri.parse(urlString);
         String userAgent = this.getClass().getSimpleName() + " -- Baking app";
-
-        mPlayer = ExoPlayerFactory.newSimpleInstance(getContext(), new DefaultTrackSelector());
 
         MediaSource mediaSource = new ExtractorMediaSource(uri,
                 new DefaultDataSourceFactory(getContext(), userAgent),
@@ -156,14 +160,12 @@ public class StepFragment extends Fragment {
         mPlayer.prepare(mediaSource, true, true);
 
         mPlayerView.setPlayer(mPlayer);
-        mPlayerView.setVisibility(View.VISIBLE);
+        mPlayerContainer.setVisibility(View.VISIBLE);
     }
 
-    private static Bundle buildArgsBundle(List<Step> steps, int position) {
-        Bundle args = new Bundle();
-        args.putParcelableArrayList(KEY_ARG_STEPS, new ArrayList<>(steps));
-        args.putInt(KEY_ARG_POSITION, position);
-        return args;
+    private void hideVideo() {
+        mPlayer.setPlayWhenReady(false);
+        mPlayerContainer.setVisibility(View.GONE);
     }
 
     private List<Step> getSteps(Bundle args, Bundle state) {
@@ -188,11 +190,13 @@ public class StepFragment extends Fragment {
 
     @OnClick(R.id.prev_step)
     void onPrev() {
+        hideVideo();
         setPosition(mPosition - 1);
     }
 
     @OnClick(R.id.next_step)
     void onNext() {
+        hideVideo();
         setPosition(mPosition + 1);
     }
 }
